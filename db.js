03227e6eafb5deec415de517969e8e6cb3793438
Alexandre Tavares <prodalexandretavares@gmail.com>
2023-01-24 16:42:36 +0000

    mysql import and code mock

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+
+//TODO Move credentials to CONTANTS file
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+//Example of db query
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
