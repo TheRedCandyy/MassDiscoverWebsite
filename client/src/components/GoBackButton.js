ca90ea57e8552fc91149b1eb20ca87641f6bf4bd
JoaoBorges46 <joborgess@hotmail.com>
2023-05-14 00:13:49 +0100

    MD-018:ADD: Implemented Search Page go back CTA

---
+++ client/src/components/GoBackButton.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function GoBackButton(props) {
+    const handleGoBack = (e) => {
+        e.preventDefault();
+
+        //Scrolls left
+        const element = document.getElementById('landingPage');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+    return (
+        <div className="sm:mr-28 sm:mt-14 sm:w-40 mr-6 mt-10">
+            <button type="button" onClick={handleGoBack} class="text-white bg-gray-900 border focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm pl-1 pr-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <span className="hidden sm:block ml-2">Go Back</span> 
+                <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
+        </div>
+        
+    );
+}
+
+export default GoBackButton
--- client/src/components/InfoText.js
+++ client/src/components/InfoText.js
@@ -3,12 +3,12 @@ import React from "react"
 function InfoText(props) {
     console.log(props);
     return (
-        <div className="sm:container sm:mt-14 sm:ml-28 ">
-            <p className="text-3xl sm:text-3xl sm:mb-5">
-                Searched domain: <span className="font-black ml-1">{props.reqResponseData.query}</span>
+        <div className="sm:container sm:mt-14 sm:ml-28 ml-6 mt-10">
+            <p className="text-2xl mb-2 sm:text-3xl sm:mb-5">
+                <span className="hidden sm:block">Searched domain:<span className="font-black sm:ml-2">{props.reqResponseData.query}</span></span> <span className="sm:hidden font-black sm:ml-1">{props.reqResponseData.query}</span>
             </p>
             <p className="text-base">
-                Number of subdomains found: <span className="font-black ml-1">{props.reqResponseData.subdomains.length}</span>
+                Num of subdomains found: <span className="font-black ml-1">{props.reqResponseData.subdomains?.length}</span>
             </p>
         </div>
     );
