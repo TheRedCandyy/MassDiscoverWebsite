3c3542ae0d6c6a75ad5892a9fdc8322937cc470a
Alexandre Tavares <prodalexandretavares@gmail.com>
2023-05-16 16:27:51 +0100

    MD-017:ADD: Fixed some issue and cleaned code

--- client/src/components/InfoText.js
+++ client/src/components/InfoText.js
@@ -1,14 +1,15 @@
 import React from "react"
 
 function InfoText(props) {
-    console.log(props);
+    const domainSearched = props.reqResponseData.query;
+    const subdomainTotal = props.reqResponseData.subdomains?.length
     return (
         <div className="sm:container sm:mt-14 sm:ml-28 ">
             <p className="text-3xl sm:text-3xl sm:mb-5">
-                Searched domain: <span className="font-black ml-1">{props.reqResponseData.query}</span>
+                Searched domain: <span className="font-black ml-1">{domainSearched}</span>
             </p>
             <p className="text-base">
-                Number of subdomains found: <span className="font-black ml-1">{props.reqResponseData.subdomains.length}</span>
+                Number of subdomains found: <span className="font-black ml-1">{subdomainTotal ?  subdomainTotal : null}</span>
             </p>
         </div>
     );
