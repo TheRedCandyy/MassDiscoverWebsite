c5b0bf39e2f9c81271ee838e3eca9324e3f2d83f
Alexandre Tavares <prodalexandretavares@gmail.com>
2023-05-16 16:46:43 +0100

    MD-018:ADD: Fixed bugs, cleaned code.

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,33 @@ import Navbar from "./components/Navbar"
 import LandingText from "./components/LandingText"
 import MetricsBoxes from "./components/MetricsBoxes";
 import SearchPage from "./components/SearchPage";
-import {Helmet} from "react-helmet";
+import {Helmet, HelmetProvider } from "react-helmet-async";
 
 function App(props) {
   const [reqResponse, setReqResponse] = useState({});
   const resRequestData = (data) => {
       setReqResponse(data);
   };
+
   return (
-    <div className="bg-zinc-900 text-white flex flex-row min-w-max">
-      <Helmet>
-        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <title>Mass Discover</title>
-      </Helmet>
-      <div className="min-h-screen w-screen">
-        <Navbar dataScreen={'landingPage'}></Navbar>
-        <LandingText handleResRequestData={resRequestData}></LandingText>
-        <MetricsBoxes></MetricsBoxes>
+    <HelmetProvider>
+      <div className="bg-zinc-900 text-white flex flex-row min-w-max">
+        <Helmet>
+          <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <title>Mass Discover</title>
+        </Helmet>
+        <div className="min-h-screen w-screen">
+          <Navbar dataScreen={'landingPage'}></Navbar>
+          <LandingText handleResRequestData={resRequestData}></LandingText>
+          <MetricsBoxes></MetricsBoxes>
+        </div>
+        <div className="min-h-screen w-screen">
+          <Navbar dataScreen={'searchPage'}></Navbar>
+          <SearchPage reqResponseData={reqResponse}></SearchPage>
+        </div>
       </div>
-      <div className="min-h-screen w-screen">
-        <Navbar dataScreen={'searchPage'}></Navbar>
-        <SearchPage reqResponseData={reqResponse}></SearchPage>
-      </div>
-    </div>
+    </HelmetProvider>
   );
 }
 
